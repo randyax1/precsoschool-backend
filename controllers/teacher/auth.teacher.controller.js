de0c1287ff06f5d3a9811af1761470e0113373b7
randyax1 <44277834+randyax1@users.noreply.github.com>
2022-01-22 11:05:57 -0700

    PSBE-2| Encriptacion de password

--- controllers/teacher/auth.teacher.controller.js
+++ controllers/teacher/auth.teacher.controller.js
@@ -1,37 +1,89 @@
+const bcrypt = require("bcryptjs");
+
 const userTeacherModel = require("../../models/teacher/user.teacher.model");
 
 const createTeacher = async (req, res) => {
 
-  //const { rol = "Docente", name, lastName, email, password } = req.body;
+  const { email, password } = req.body;
 
   try {
+  
+    let user = await userTeacherModel.findOne({ email });
 
-    const user = new userTeacherModel(req.body);
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Correo ya registrado anteriormente.",
+      });
+    }
+
+    user = new userTeacherModel(req.body);
+
+    //Encriptacion contraseña
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
 
     await user.save();
 
     res.status(201).json({
       ok: true,
-      msg: "Registered teacher",
+      uid: user.id,
+      rol: user.rol,
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      password: user.password,
     });
 
   } catch (error) {
-      res.status(500).json({
-          ok: false,
-          msg:'Porfavor contacte al administrador.'
-      })
+    res.status(500).json({
+      ok: false,
+      msg: "Porfavor contacte al administrador.",
+    });
+    
   }
 };
 
-const loginTeacher = (req, res) => {
+const loginTeacher = async (req, res) => {
+  
   const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: "Teacher Logged",
-    email,
-    password,
-  });
+  try {
+  
+    const user = await userTeacherModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario o contraseña no son correctos.",
+      });
+    }
+
+    //Confirmacion de passwords
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Contraseña incorrecta.",
+      });
+    }
+
+    //Generar JWT
+
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  
+  } catch (error) {
+  
+    res.status(500).json({
+      ok: false,
+      msg: "Porfavor contacte al administrador.",
+    });
+  }
 };
 
 const renewToken = (req, res) => {
